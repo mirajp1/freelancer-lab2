c7ca5d42a28eeec04ac61b7d78379c7480c4e6f0
mirajp1 <mirajp1@gmail.com>
2018-04-14 13:13:42 -0700

    removed sequelize

--- freelancer-kafka-backend/kafka/Connection.js
+++ freelancer-kafka-backend/kafka/Connection.js
@@ -4,7 +4,7 @@ function ConnectionProvider() {
     this.getConsumer = function(topic_name) {
         if (!this.kafkaConsumerConnection) {
 
-            this.client = new kafka.Client("localhost:2181");
+            this.client = new kafka.Client("54.67.38.71:2181");
             this.kafkaConsumerConnection = new kafka.Consumer(this.client,[ { topic: topic_name, partition: 0 }],{groupId: topic_name});
             this.client.on('ready', function () { console.log('client ready!') })
             this.client.on('error', function (err) { console.log(topic_name+': error');console.log(err) })
@@ -16,7 +16,7 @@ function ConnectionProvider() {
     this.getProducer = function() {
 
         if (!this.kafkaProducerConnection) {
-            this.client = new kafka.Client("localhost:2181");
+            this.client = new kafka.Client("54.67.38.71:2181");
             var HighLevelProducer = kafka.HighLevelProducer;
             this.kafkaProducerConnection = new HighLevelProducer(this.client);
             //this.kafkaConnection = new kafka.Producer(this.client);
